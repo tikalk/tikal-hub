71b836b15554b68219245b1cd3ea6235c0856318
Igor Zelmanovich <igor.israel@gmail.com>
2014-07-28 10:56:36 +0300

    add settings activity

--- android/app/src/main/java/com/tikalk/tikalhub/MainActivity.java
+++ android/app/src/main/java/com/tikalk/tikalhub/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tikalk.tikalhub;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,6 +30,7 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
