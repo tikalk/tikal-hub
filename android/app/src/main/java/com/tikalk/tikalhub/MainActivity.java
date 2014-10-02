bf9e5765af8e2145ed9df2f03f8bfe99f8497687
Igor Zelmanovich <igor.israel@gmail.com>
2014-10-02 12:44:07 +0300

    starting with navigation drawer

--- android/app/src/main/java/com/tikalk/tikalhub/MainActivity.java
+++ android/app/src/main/java/com/tikalk/tikalhub/MainActivity.java
@@ -2,13 +2,18 @@ package com.tikalk.tikalhub;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -25,11 +30,16 @@ public class MainActivity extends Activity {
     private ProgressBar progressBar;
     private FeedAggregator feedAggregator;
     private FeedAggregator.Listener feedAggregatorListener;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initDrawer();
 
         feedAggregator = FeedAggregator.getInstance();
 
@@ -74,6 +84,101 @@ public class MainActivity extends Activity {
         feedAggregator.addListener(feedAggregatorListener);
     }
 
+    private void initDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        final ListView drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        drawerList.setAdapter(new ArrayAdapter<NavigationMenuItem>(this,
+                R.layout.drawer_list_item, new NavigationMenuItem[]{
+                new NavigationMenuItem("menu_news_feed", "News Feed"),
+                new NavigationMenuItem("menu_settings", "Settings")
+        }));
+        // Set the list's click listener
+        drawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                if (onDrawerItemSelected(position)) {
+                    // Highlight the selected item, update the title, and close the drawer
+                    drawerList.setItemChecked(position, true);
+                }
+
+                drawerLayout.closeDrawer(drawerList);
+            }
+        });
+
+    }
+
+    class NavigationMenuItem {
+        private String id;
+        private String title;
+
+        NavigationMenuItem(String id, String title) {
+            this.id = id;
+            this.title = title;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String toString() {
+            return getTitle();
+        }
+    }
+
+    private boolean onDrawerItemSelected(int position) {
+        return false;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -108,11 +213,30 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = drawerLayout.isDrawerOpen(Gravity.START);
+
+        menu.findItem(R.id.action_refresh).setVisible(!drawerOpen);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         int id = item.getItemId();
         switch (item.getItemId()) {
             case R.id.action_settings:
